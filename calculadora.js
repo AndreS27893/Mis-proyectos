9c41cfa02205122190fb8ecc4ce170cda0dbeff0
AndreS27893 <andrescba27893@gmail.com>
2022-03-23 22:38:10 -0300

    Cambios en funcionalidades
    Cambios en los botones y en lo que tiene que imprimir consola para que la maquina entiende que esta aprentando un "numero".

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,11 @@
-getElementById("container").addEventListener("transitionend", updateTransition, true);
+window.addEventListener('load',()=>{
+    const display= document.querySelector(["calculadora-display"]);
+    const keypadButtons= document.getElementsByClassName('botones-de-numeros');  
+    
+    const keypadButtonsArray= Array.from(keypadButtons);
+    keypadButtonsArray.forEach( (button) => {
+        button.addEventListener("click",()=>{
+            console.log(button.innerHTML)
+        })
+    });
+});
